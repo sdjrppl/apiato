63a729264fead7199630e6854d8a408c61acadf3
Joao Prizal <joao@smartdigit.pt>
2020-09-21 15:31:23 +0100

    Add apiato user model namespace

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model'  => env('USER_NAMESPACE') . User::class,
         ],
 
         // 'users' => [
